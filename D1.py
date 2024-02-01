752de441244b92aaf9cf77fd2e35248fc89d81a3
Vishakha Juneja <vishakhaj1092@gmail.com>
2024-02-01 08:31:42 +0530

    test

--- D1.py
+++ D1.py
@@ -1,4 +1,44 @@
-a=1
-b=30
-total = a+b
-print(total)
+# #Addition
+# a=1
+# b=30
+# total = a+b
+# print(total)
+
+# #Subtraction
+# x=30
+# y=10
+# sub = x-y
+# print(sub)
+
+# # Subtraction 2 
+# Sub1 = [35,30,28]
+# Sub2 = [20,10,12]
+# Sub = Sub1[0]-Sub2[2]
+# print(Sub)
+
+# #Division 
+# D1 = 120
+# D2 = 2 
+# Div = D1/D2
+# print(Div)
+
+# #Modulus 
+# M1 = 121
+# M2 = 2
+# Mod = M1%M2
+# print(Mod)
+
+# #multiplication
+# Mul1 = 5
+# Mul2 = 6
+# Mult = Mul1*Mul2
+# print(Mult)
+
+# #BODMAS
+# Var1 = 4
+# Var2 = 9
+# Var3 = 34
+# Var4 = 78
+# Var5 = 89
+# BoD = Var3-(Var4+Var1)/Var2%Var5
+# print(BoD)
