1795c5aecfdb86cf1fb9f1054bbbfc05407b4634
Vishakha Juneja <vishakhaj1092@gmail.com>
2024-02-05 22:30:33 +0530

    Day3

---
+++ D3.py
@@ -0,0 +1,48 @@
+#***Maths function***
+
+import math
+
+var1 = 2.7
+# print(round(var1))
+# print(math.floor(var1))
+# print(math.ceil(var1))
+var2 = 4 
+#print(math.pow(var2,4)) #Power = pow
+#print(math.sqrt(var2))
+
+#***String functions:***
+
+long = "vishakha sandeep Virendra Juneja"
+# print(long.upper())
+# print(long.lower())
+# print(long.capitalize())
+# print(long.title())
+
+long1 = "Hello, veronica is eagerly waiting for the miracle to happen in this life"
+# print(long+ long1)
+# long2 = "Hello"
+# long3 = long2 in long1
+# print(long3)
+# long4 = long1.startswith('hello')
+# print(long4)
+# long5 = long1.endswith('life')
+# print(long5)
+# print(long1[0:5])
+# print(long1[-5:-1])
+# print(long1[-1:])
+#print(long1.find('w'))  #To find the index in the string 
+# print(long1.count('a'))
+# print(long1.replace('Hello','Hey'))
+# print(long1.split(','))
+# print(long1.split('is'))
+
+#***List functions : list uses square [] bracket ***
+
+long5 = long1.split(' ')
+print(long5)
+# print(len(long5))
+# long6 = long5.append('test')
+# print(long5)
+# extending = ['test1', 'test2', 'test3']
+# long6 = long5.extend(extending)
+# print(long5)
